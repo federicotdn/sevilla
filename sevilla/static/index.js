fb9f4db734dec00cc52e8d92f28f7ca48df7d9a8
Federico Tedin <federicotedin@gmail.com>
2019-09-27 02:36:09 +0200

    Simplify js

--- sevilla/static/index.js
+++ sevilla/static/index.js
@@ -1,110 +1,84 @@
 "use strict";
 
-const OK_COLOR = "#28A745";
-const LOAD_COLOR = "#A0A0A0";
-const ERR_COLOR = "#CC3232";
 const SEND_INTERVAL_MS = 1 * 1000;
 const RETRY_INTERVAL_MS = 4 * 1000;
 const STORAGE_KEY = "unsent_note";
 
-var indicatorOk = true;
-var noteId = null;
-var noteElem = null;
+var Colors = Object.freeze({
+    ok: "#28A745",
+    loading: "#A0A0A0",
+    error: "#CC3232"
+});
+
 var lastTimeout = null;
 var lastTimestamp = null;
-var indicatorElem = null;
+
+function elem(id) { return document.getElementById(id); }
 
 window.addEventListener("DOMContentLoaded", () => {
-    noteId = document.getElementById("noteId").innerText;
-
-    noteElem = document.getElementById("noteText");
-    noteElem.oninput = noteModified;
-
-    indicatorElem = document.getElementById("indicator");
+    elem("noteText").oninput = noteModified;
 
     var unsentNote = localStorage.getItem(STORAGE_KEY);
     if (unsentNote) {
-	// Restore unsent note and try to send it
-	noteElem.value = unsentNote;
-	noteModified();
+        // Restore unsent note and try to send it
+        elem("noteText").value = unsentNote;
+        noteModified();
     } else {
-	noteElem.value = "";
-	// Note is blank - and it doesn't exist on the server yet, so
-	// technically we're synced
-	setIndicatorColor(OK_COLOR);
+	elem("indicator").style.backgroundColor = Colors.ok;
     }
 });
 
-function setIndicatorColor(color) {
-    indicatorElem.style.backgroundColor = color;
-    indicatorOk = (color === OK_COLOR);
-}
-
 function noteModified() {
-    setIndicatorColor(LOAD_COLOR);
+    elem("indicator").style.backgroundColor = Colors.loading;
     scheduleUploadNote(SEND_INTERVAL_MS);
 }
 
 function scheduleUploadNote(interval) {
     if (lastTimeout) {
-	clearTimeout(lastTimeout);
-	lastTimeout = null;
+        clearTimeout(lastTimeout);
+        lastTimeout = null;
     }
 
-    localStorage.setItem(STORAGE_KEY, noteElem.value);
+    localStorage.setItem(STORAGE_KEY, elem("noteText").value);
 
     // Schedule a new send
     lastTimeout = setTimeout(() => {
-	lastTimeout = null;
-	uploadNote();
+        lastTimeout = null;
+        uploadNote();
     }, interval);
 }
 
 function uploadNote() {
-    var timestamp = Date.now();
-    var params = new URLSearchParams({
-	"timestamp": timestamp
+    lastTimestamp = Date.now();
+
+    const url = "/notes/" + elem("noteId").innerText + "?" + "timestamp=" + lastTimestamp.toString();
+
+    function status(response) {
+        if (response.status === 200) {
+            return Promise.resolve(response);
+        }
+        return Promise.reject(new Error("Unable to upload note."));
+    }
+
+    function json(response) {
+        return response.json();
+    }
+
+    fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: { "Content-type": "text/plain" },
+        body: elem("noteText").value
+    }).then(status).then(json).then((data) => {
+        var receivedTimestamp = data.timestamp;
+
+        if (lastTimestamp === receivedTimestamp) {
+            elem("indicator").style.backgroundColor = Colors.ok;
+            lastTimestamp = null;
+            localStorage.removeItem(STORAGE_KEY);
+        }
+    }).catch(() => {
+        elem("indicator").style.backgroundColor = Colors.error;
+	scheduleUploadNote(RETRY_INTERVAL_MS);
     });
-
-    var request = new XMLHttpRequest();
-    request.open('POST', "/notes/" + noteId + "?" + params.toString());
-    request.setRequestHeader("Content-Type", "text/plain");
-
-    request.onload = noteUploaded;
-    request.onerror = noteUploadedError;
-
-    lastTimestamp = timestamp;
-    request.send(noteElem.value);
-}
-
-function noteUploaded() {
-    /* jshint validthis: true */
-
-    if (this.status !== 200) {
-	if (this.status === 401) {
-	    // The user supposedly got to the current page by logging
-	    // in, but after trying to submit a note, got an
-	    // authentication error.  I've only seen this happen with
-	    // iOS Web Applications, where sometimes index.html is
-	    // cached and the login is skipped. Refresh the page to
-	    // get a new login.
-	    location.reload();
-	}
-	noteUploadedError();
-	return;
-    }
-
-    var data = JSON.parse(this.response);
-    var receivedTimestamp = data.timestamp;
-
-    if (lastTimestamp === receivedTimestamp) {
-	setIndicatorColor(OK_COLOR);
-	lastTimestamp = null;
-	localStorage.removeItem(STORAGE_KEY);
-    }
-}
-
-function noteUploadedError() {
-    setIndicatorColor(ERR_COLOR);
-    scheduleUploadNote(RETRY_INTERVAL_MS);
 }
