691e246889cfa3f2f13b082cefdf67bc1992aaec
Federico Tedin <federicotedin@gmail.com>
2019-11-25 02:16:41 +0100

    Fix Docker tests

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -12,17 +12,17 @@ class TestDockerService(unittest.TestCase):
         note_text = "Hello, World!"
 
         # Try creating a note before logging in
-        resp = s.post(
+        resp = s.put(
             BASE_URL + "/notes/" + note_id, params={"timestamp": 2}, data=note_text
         )
         self.assertEqual(resp.status_code, 401)
 
         # Log in
-        resp = s.post(BASE_URL + "/login", data={"password": "sevilla"})
+        resp = s.post(BASE_URL + "/session/login", data={"password": "sevilla"})
         self.assertEqual(resp.status_code, 200)
 
         # Create first version of note
-        resp = s.post(
+        resp = s.put(
             BASE_URL + "/notes/" + note_id, params={"timestamp": 1000}, data=note_text
         )
         self.assertEqual(resp.status_code, 200)
@@ -33,7 +33,7 @@ class TestDockerService(unittest.TestCase):
         self.assertTrue(note_text in resp.text)
 
         # Try updating it with an older timestamp
-        resp = s.post(
+        resp = s.put(
             BASE_URL + "/notes/" + note_id, params={"timestamp": 500}, data="Testing"
         )
         self.assertEqual(resp.status_code, 200)
@@ -46,7 +46,7 @@ class TestDockerService(unittest.TestCase):
         note_text = "FoobarFoobar"
 
         # Actually update the note's text
-        resp = s.post(
+        resp = s.put(
             BASE_URL + "/notes/" + note_id, params={"timestamp": 2000}, data=note_text
         )
         self.assertEqual(resp.status_code, 200)
@@ -57,11 +57,11 @@ class TestDockerService(unittest.TestCase):
         self.assertTrue(note_text in resp.text)
 
         # Log out
-        resp = s.post(BASE_URL + "/logout")
+        resp = s.post(BASE_URL + "/session/logout")
         self.assertEqual(resp.status_code, 200)
 
         # Try updating a note after logging out
-        resp = s.post(
+        resp = s.put(
             BASE_URL + "/notes/" + note_id, params={"timestamp": 2}, data=note_text
         )
         self.assertEqual(resp.status_code, 401)
